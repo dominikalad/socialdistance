b56d6bccd425e798fcbae125e049df7f5c21ae84
Dominika <dominika.ladziak@rsqtechnologies.com>
2021-01-09 10:35:30 -0800

    Refactor

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyDuFYOSvz6hsFv1CHWAmo5QCRF904T302M",
+    authDomain: "socialdistance-78857.firebaseapp.com",
+    databaseURL: "https://socialdistance-78857-default-rtdb.firebaseio.com",
+    projectId: "socialdistance-78857",
+    storageBucket: "socialdistance-78857.appspot.com",
+    messagingSenderId: "94021794240",
+    appId: "1:94021794240:web:9a2648b1fbc233b1793025",
+    measurementId: "G-7MK12Q84KG"
+};
